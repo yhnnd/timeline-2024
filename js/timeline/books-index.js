6001bede22345e10177efc121cd607bc54aabe36
yhnnd <7866938+yhnnd@users.noreply.github.com>
2024-07-17 03:50:12 +0800

    up

--- js/timeline/books-index.js
+++ js/timeline/books-index.js
@@ -18,6 +18,7 @@ window.books[3].indexList = [
     "../yhnnd.wordpress.com/3-diary-2024-07-2024-12-出路5/2024.07.14.txt",
     "../yhnnd.wordpress.com/3-diary-2024-07-2024-12-出路5/2024.07.15.txt",
     "../yhnnd.wordpress.com/3-diary-2024-07-2024-12-出路5/2024.07.16.txt",
+    "../yhnnd.wordpress.com/3-diary-2024-07-2024-12-出路5/2024.07.17.txt",
 ];
 
 // [{
