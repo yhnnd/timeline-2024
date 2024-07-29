ea06341ccc7b73654c6a2325ffd4ec9018ecb1e0
yhnnd <7866938+yhnnd@users.noreply.github.com>
2024-07-29 10:19:04 +0800

    Update books-index.js

--- js/timeline/books-index.js
+++ js/timeline/books-index.js
@@ -535,7 +535,7 @@ window.books[20].indexList = [
     "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.07.20.txt",
     "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.07.22.txt",
     "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.07.24.txt",
-    "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.07.29.txt",
+    "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.07.29.txt"
 ];
 
 // 21 詩 (2024)
