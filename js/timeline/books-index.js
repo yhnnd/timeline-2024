82c348ed575e17e06a3444d2ea0b88abdedbb115
user <user@example.com>
2024-08-24 17:25:42 +0800

    up

--- js/timeline/books-index.js
+++ js/timeline/books-index.js
@@ -545,7 +545,8 @@ window.books[20].indexList = [
     "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.08.10.txt",
     "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.08.11.txt",
     "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.08.12.txt",
-    "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.08.19.txt"
+    "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.08.19.txt",
+    "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.08.24.txt"
 ];
 
 // 21 詩 (2024)
