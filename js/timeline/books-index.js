ffff50f289ee5d0ca1406a0bb4840d01c03a238b
yhnnd <7866938+yhnnd@users.noreply.github.com>
2024-11-01 00:37:32 +0800

    up

--- js/timeline/books-index.js
+++ js/timeline/books-index.js
@@ -566,6 +566,7 @@ window.books[20].indexList = [
     "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.10.26.txt",
     "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.10.28.txt",
     "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.10.29.txt",
+    "@2024/yhnnd.wordpress.com/20-diary-2024-07-2024-12-出路5/2024.10.30.txt",
 ];
 
 // 21 詩 (2024)
